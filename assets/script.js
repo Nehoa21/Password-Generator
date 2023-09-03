47efaf7711dac0e173b510009d530dd0c2c4f5c4
Nehoa21 <nehoaakina21@yahoo.com>
2023-09-03 15:33:02 -0600

    moved code from develop folder, added arrays for characters

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+//create variables that hold arrays of different types of characters
+
+//need a function called generate password
+
+var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = [127];
+var specialChar = ["~","!","@","#","$","%","^","&","*","(",")","_","+","-","="];
+
+
+
+
+
+
+
+
+
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
