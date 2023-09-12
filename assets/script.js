66368e59f2018456e3e3fc7bce211ed67901b705
Nehoa21 <nehoaakina21@yahoo.com>
2023-09-11 22:40:27 -0600

    tested more options to get final result, still nothing. will turn in and finish later with help

--- assets/script.js
+++ assets/script.js
@@ -8,10 +8,15 @@ var lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n",
 var upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var specialChar = ["~","!","@","#","$","%","^","&","*","(",")","_","+","-","=","[","]","|",";","'",":",",",".","/","<",">","?"];
 var numbers = ["1","2","3","4","5","6","7","8","9","0"];
+// var passLength = 0;
+// var incLowerCase = false;
+// var incUpperCase = false;
+// var incSpecial = false;
+// var incNums = false;
 
 //function for the window prompts
 var windPrompts = function (){
-  var passLength = window.prompt("What length of password do you want? Min: 8 Max: 127");
+  var passLength = window.prompt("Input the length of password desired. Min: 8 Max: 127");
   if (passLength >= 8 && passLength <= 127) {
     window.alert("Password length will be " + passLength + " characters long.");
   } else {
@@ -19,85 +24,85 @@ var windPrompts = function (){
     return;
   }
 
-  var incLowerCase = window.confirm("Do you want to add lowercase letters? OK: yes, Cancel: no");
+  var incLowerCase = window.confirm("Would you like to have lowercase letters? OK: yes, Cancel: no");
   if (incLowerCase === true) {
     window.alert("Include lowercase letters.")
   } else {
     window.alert("No lowercase letters.")
   }
 
-  var incUpperCase = window.confirm("Do you want to add uppercase letters?");
+  var incUpperCase = window.confirm("Would you like to have uppercase letters? OK: yes, Cancel: no");
   if (incUpperCase === true) {
     window.alert("Include uppercase letters.");
   } else {
     window.alert("No uppercase letters.");
   }
 
-  var incSpecial = window.confirm("Do you want to add special characters?");
+  var incSpecial = window.confirm("Would you like to have special characters? OK: yes, Cancel: no");
   if (incSpecial === true) {
     window.alert("Include special characters.");
   } else {
     window.alert("No special characters.");
   }
 
-  var incNums = window.confirm("Do you want to add numbers?");
+  var incNums = window.confirm("Would you like to have numbers? OK: yes, Cancel: no");
   if (incNums === true) {
     window.alert("Include numbers.");
   } else {
     window.alert("No numbers.");
   }
+  return;
 }
 
 //return booleans for window prompts, then make 'if' for them to include in get random function
-var userChoiceTypes = function() {
-  var userChoices = [];
-  if (windPrompts.incLowerCase || windPrompts.incUpperCase || windPrompts.incSpecial || windPrompts.incNums) {
-    userChoices.push;
-  }  
-}
+// var userChoiceTypes = function() {
+//   var userChoices = [];
+//   if (windPrompts.incLowerCase || windPrompts.incUpperCase || windPrompts.incSpecial || windPrompts.incNums) {
+//     userChoices.push;
+//   }  
+// }
 
 var randChar = function(){
   var randIndex = [];
   var randInts = [];
-  randIndex = math.floor(math.random() * passLength.length);
+  randIndex = math.floor(math.random() * windPrompts.passLength.length);
   randInts = userChoices[randIndex];
 
   return randInts;
 }
 
-//function to start the password generator, from start to finish
+//function to get the random password
 var generatePassword = function() {
   windPrompts();
-  var choices = userChoiceTypes();
-  var potentChoices = [];
-  var finalPicks = [];
+  var choices = [];
+  var potential = [];
+  var final = [];
   
   //error with making the array
   if (choices.incLowerCase) {
-    potentChoices = potentChoices.concat(lowerCaseLetters);
-    finalPicks.push(randChar(lowerCaseLetters))
+    potential = potential.concat(lowerCaseLetters);
+    final.push(randChar(lowerCaseLetters))
   }
 
-  if (choices.userChoiceTypes(incUpperCase)) {
-    potentChoices = potentChoices.concat(upperCaseLetters);
-    finalPicks.push(randChar(upperCaseLetters))
+  if (choices.incUpperCase) {
+    potential = potential.concat(upperCaseLetters);
+    final.push(randChar(upperCaseLetters))
   }
 
-  if (choices.userChoiceTypes(incSpecial)) {
-    potentChoices = potentChoices.concat(specialChar);
-    finalPicks.push(randChar(specialChar))
+  if (choices.incSpecial) {
+    potential = potential.concat(specialChar);
+    final.push(randChar(specialChar))
   }
 
-  if (choices.userChoiceTypes(incNums)) {
-    potentChoices = potentChoices.concat(numbers);
-    finalPicks.push(randChar(numbers))
+  if (choices.incNums) {
+    potential = potential.concat(numbers);
+    final.push(randChar(numbers))
   }
 
-  console.log()
-  return finalPicks;
+  console.log(final)
+  return final;
 }
   
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -107,7 +112,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
